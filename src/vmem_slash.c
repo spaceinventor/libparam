18addd9ec4dd50655c98d661badcb8fb3bc5f85c
Johan De Claville Christiansen <johan@safeex.com>
2016-10-09 00:18:49 +0200

    Add vmem_slash

---
+++ src/vmem_slash.c
@@ -0,0 +1,16 @@
+/*
+ * vmem_slash.c
+ *
+ *  Created on: Oct 8, 2016
+ *      Author: johan
+ */
+
+#include <vmem.h>
+#include <slash/slash.h>
+
+static int slash_vmem_list(struct slash *slash)
+{
+	vmem_list();
+	return SLASH_SUCCESS;
+}
+slash_command(vmem_list, slash_vmem_list, NULL, "List vmems");
