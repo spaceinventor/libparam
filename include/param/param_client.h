9aa2a48153f8ae3e6438550f86395c8766676230
Johan De Claville Christiansen <johan@safeex.com>
2017-01-24 10:29:45 +0100

    Fix beacon#

--- include/param/param_client.h
+++ include/param/param_client.h
@@ -8,11 +8,13 @@
 #ifndef LIB_PARAM_INCLUDE_PARAM_PARAM_CLIENT_H_
 #define LIB_PARAM_INCLUDE_PARAM_PARAM_CLIENT_H_
 
+#include <csp/csp.h>
 #include <param/param.h>
 
 int param_push_single(param_t * param, int host, int timeout);
 int param_push(param_t * params[], int count, int verbose, int host, int timeout);
 int param_pull_single(param_t * param, int host, int timeout);
 int param_pull(param_t * params[], int count, int verbose, int host, int timeout);
+csp_packet_t * param_pull_request(param_t * params[], int count, int host);
 
 #endif /* LIB_PARAM_INCLUDE_PARAM_PARAM_CLIENT_H_ */
