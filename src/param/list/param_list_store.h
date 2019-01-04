7de2a17e18c6ff40f083a1297062b1aafca81ffe
Johan De Claville Christiansen <johan@safeex.com>
2019-01-04 09:27:05 +0100

    Remove vmem load/save if no fopen

---
+++ src/param/list/param_list_store.h
@@ -0,0 +1,14 @@
+/*
+ * param_list_store.h
+ *
+ *  Created on: Jan 4, 2019
+ *      Author: johan
+ */
+
+#ifndef LIB_PARAM_SRC_PARAM_LIST_PARAM_LIST_STORE_H_
+#define LIB_PARAM_SRC_PARAM_LIST_PARAM_LIST_STORE_H_
+
+void param_list_store_vmem_save(const vmem_t * vmem);
+void param_list_store_vmem_load(const vmem_t * vmem);
+
+#endif /* LIB_PARAM_SRC_PARAM_LIST_PARAM_LIST_STORE_H_ */
