316df8b939148ba96c34c87b6696370e60b493fd
Jean-Baptiste Lab <jean.baptiste@space-inventor.com>
2024-03-11 10:38:24 +0100

    Grow mmap by computed required amount instead of 4Kbytes at a time (which isn't a good way anyway as the input might actually end up going beyond size+4 Kbytes...)

--- src/vmem/vmem_mmap.c
+++ src/vmem/vmem_mmap.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-static void ensure_init(vmem_mmap_driver_t *drv, uint32_t size)
+static void ensure_init(vmem_mmap_driver_t *drv, uint32_t *size)
 {
 	if (0 == drv->physaddr)
 	{
@@ -16,8 +16,15 @@ static void ensure_init(vmem_mmap_driver_t *drv, uint32_t size)
 			perror("open");
 			return;
 		}
-		ftruncate(fd, size);
-		drv->physaddr = mmap(0, size, PROT_WRITE|PROT_READ, MAP_SHARED, fd, 0);
+		long cur_size = lseek(fd, 0, SEEK_END);
+		if (cur_size < *size) {
+			/* Grow the file if needed */
+			ftruncate(fd, *size);
+		} else {
+			/* File size is >= requested size, don't destroy/truncate data but adjust the driver size instead */
+			*size = cur_size;
+		}
+		drv->physaddr = mmap(0, *size, PROT_WRITE|PROT_READ, MAP_SHARED, fd, 0);
 		close(fd);
 		if (drv->physaddr == MAP_FAILED)
 		{
@@ -30,22 +37,22 @@ static void ensure_init(vmem_mmap_driver_t *drv, uint32_t size)
 void vmem_mmap_read(vmem_t *vmem, uint32_t addr, void *dataout, uint32_t len)
 {
 	vmem_mmap_driver_t *drv = (vmem_mmap_driver_t *)vmem->driver;
-	ensure_init(drv, vmem->size);
+	ensure_init(drv, &vmem->size);
 	memcpy(dataout, ((vmem_mmap_driver_t *)vmem->driver)->physaddr + addr, len);
 }
 
 void vmem_mmap_write(vmem_t *vmem, uint32_t addr, const void *datain, uint32_t len)
 {
 	vmem_mmap_driver_t *drv = (vmem_mmap_driver_t *)vmem->driver;
-	ensure_init(drv, vmem->size);
+	ensure_init(drv, &vmem->size);
 	if ((addr + len) >  vmem->size) {
 		// Grow the file...
 		if (msync(drv->physaddr, vmem->size, MS_SYNC|MS_INVALIDATE) == -1) {
 			perror("Could not sync the file to disk");
 		}
-		vmem->size += 4096 * 1000;
+		vmem->size = (addr + len);
 		drv->physaddr = 0;
-		ensure_init(drv, vmem->size);
+		ensure_init(drv, &vmem->size);
 	}
 	memcpy(drv->physaddr + addr, datain, len);
 }
