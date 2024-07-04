80cff12e254093e4bd9c80c87d451f32a63d5883
Thomas Lykkeberg <thomas.lykkeberg@space-inventor.com>
2024-07-04 08:59:41 +0200

    SI-4694 Updated the vmem file and mmap drivers to support new 64-bit API.

--- src/vmem/vmem_client.c
+++ src/vmem/vmem_client.c
@@ -161,7 +161,7 @@ int vmem_upload(int node, int timeout, uint64_t address, char * datain, uint32_t
 	if(count != length){
 		unsigned int window_size = 0;
 		csp_rdp_get_opt(&window_size, NULL, NULL, NULL, NULL, NULL);
-		printf("Upload didn't complete, suggested offset to resume: %lu\n", count - ((window_size + 1) * VMEM_SERVER_MTU));
+		printf("Upload didn't complete, suggested offset to resume: %u\n", count - ((window_size + 1) * VMEM_SERVER_MTU));
 		return -1;
 	} else {
 		printf("  Uploaded %u bytes in %.03f s at %u Bps\n", (unsigned int) count, time_total / 1000.0, (unsigned int) (count / ((float)time_total / 1000.0)) );
