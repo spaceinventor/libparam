fe9998ec60efca41dc85ec6939c74145876df934
Troels Jessen <troels@space-inventor.com>
2024-09-30 09:53:21 +0200

    Check for invalid ascii sequences when writing strings of type DATA

--- src/param/param_string.c
+++ src/param/param_string.c
@@ -168,16 +168,23 @@ int param_str_to_value(param_type_e type, char *in, void *out) {
 	case PARAM_TYPE_DATA: {
 		int len = strlen(in) / 2;
 
-		unsigned int nibble(char c) {
+		if (2*len != strlen(in))
+			return -1;
+
+		int nibble(char c) {
 			if (c >= '0' && c <= '9') return      c - '0';
 			if (c >= 'A' && c <= 'F') return 10 + c - 'A';
 			if (c >= 'a' && c <= 'f') return 10 + c - 'a';
 			return -1;
 		}
 
-		for (int i = 0; i < len; i++)
-			((char *) out)[i] = (nibble(in[i*2]) << 4) + nibble(in[i*2+1]);
-			
+		for (int i = 0; i < len; i++) {
+			int nibble1 = nibble(in[i*2]);
+			int nibble2 = nibble(in[i*2+1]);
+			if (nibble1 < 0 || nibble2 < 0) return -1;
+			((char *) out)[i] = (nibble1 << 4) + nibble2;
+		}
+
 		return len;
 	}
 
