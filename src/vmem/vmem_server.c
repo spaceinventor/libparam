522dfb4dcc644b6646cb980683671ed44924c113
Troels Jessen <troels@space-inventor.com>
2025-01-07 09:07:20 +0100

    FIx buffer leak

--- src/vmem/vmem_server.c
+++ src/vmem/vmem_server.c
@@ -282,12 +282,13 @@ static void rparam_list_handler(csp_conn_t * conn)
 	param_t * param;
 	param_list_iterator i = {};
 	while ((param = param_list_iterate(&i)) != NULL) {
-		csp_packet_t * packet = csp_buffer_get(256);
-		if (packet == NULL)
-		    break;
 
 		if (param->mask & PM_REMOTE && *param->node == 0)
 			continue;
+
+		csp_packet_t * packet = csp_buffer_get(256);
+		if (packet == NULL)
+		    break;
 
 		memset(packet->data, 0, 256);
 
