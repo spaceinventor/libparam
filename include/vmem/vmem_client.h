3ef000b27a0271b3f86f35fff30e15cd14ba6f53
Jean-Baptiste Lab <jeanbaptiste.lab@gmail.com>
2025-10-21 10:32:31 +0200

    Update comments

--- include/vmem/vmem_client.h
+++ include/vmem/vmem_client.h
@@ -12,9 +12,29 @@ typedef void (*vmem_progress_cb)(uint32_t total, uint32_t sofar);
  */
 void vmem_progress(uint32_t total, uint32_t sofar);
 
-
+/**
+ * @brief Download content of given VMEM address of given node to given memory
+ * @param node CSP address of recipient (a VMEM server must be running on this node)
+ * @param timeout CSP timeout to use
+ * @param address source VMEM address
+ * @param datain pointer to data buffer to store downloaded data
+ * @param length how many bytes should be downloaded
+ * @param version VMEM version
+ * @param cb optional function that will be called regularly to report progress
+ * @return < 0 in case of failure (CSP_ERR_TIMEDOUT, CSP_ERR_NOBUFS), uploaded amount of bytes otherwise (may be less then the length parameter if transfer was interrupted)
+ */
 int vmem_download_progress(int node, int timeout, uint64_t address, uint32_t length, char * dataout, int version, int use_rdp, vmem_progress_cb cb);
 
+/**
+ * @brief Download content of given VMEM address of given node to given memory
+ * @param node CSP address of recipient (a VMEM server must be running on this node)
+ * @param timeout CSP timeout to use
+ * @param address source VMEM address
+ * @param datain pointer to data buffer to store downloaded data
+ * @param length how many bytes should be downloaded
+ * @param version VMEM version
+ * @return < 0 in case of failure (CSP_ERR_TIMEDOUT, CSP_ERR_NOBUFS), uploaded amount of bytes otherwise (may be less then the length parameter if transfer was interrupted)
+ */
 int vmem_download(int node, int timeout, uint64_t address, uint32_t length, char * dataout, int version, int use_rdp);
 
 /**
@@ -25,11 +45,21 @@ int vmem_download(int node, int timeout, uint64_t address, uint32_t length, char
  * @param datain pointer to data to upload
  * @param length how many bytes should be uploaded
  * @param version VMEM version
- * @param vmem_upload_progress_cb optional function that will be called regularly to report progress
- * @return < 0 in case of failure (CSP_ERR_TIMEDOUT,  CSP_ERR_NOBUFS), uploaded amount of bytes otherwise (may be less then the length parameter if transfer was interrupted)
+ * @param cb optional function that will be called regularly to report progress
+ * @return < 0 in case of failure (CSP_ERR_TIMEDOUT, CSP_ERR_NOBUFS), uploaded amount of bytes otherwise (may be less then the length parameter if transfer was interrupted)
  */
 int vmem_upload_progress(int node, int timeout, uint64_t address, char * datain, uint32_t length, int version, vmem_progress_cb progress_cb);
 
+/**
+ * @brief Upload content of given memory to given VMEM address of given node
+ * @param node CSP address of recipient (a VMEM server must be running on this node)
+ * @param timeout CSP timeout to use
+ * @param address destination VMEM address
+ * @param datain pointer to data to upload
+ * @param length how many bytes should be uploaded
+ * @param version VMEM version
+ * @return < 0 in case of failure (CSP_ERR_TIMEDOUT, CSP_ERR_NOBUFS), uploaded amount of bytes otherwise (may be less then the length parameter if transfer was interrupted)
+ */
 int vmem_upload(int node, int timeout, uint64_t address, char * datain, uint32_t length, int version);
 
 void vmem_client_list(int node, int timeout, int version);
