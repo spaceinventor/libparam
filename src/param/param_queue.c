380ebdae92c027cbdbcbe714ee38bb1cb957f415
Henrik Davidsen <henrik.davidsen@space-inventor.com>
2026-02-09 13:27:47 +0100

    Initialise queue tv_nsec to CSP_TIMESTAMP_INVALID_NSEC

--- src/param/param_queue.c
+++ src/param/param_queue.c
@@ -36,9 +36,9 @@ void param_queue_init(param_queue_t *queue, void *buffer, int buffer_size, int u
 	queue->used = used;
 	queue->version = version;
 	queue->last_timestamp.tv_sec = 0;
-	queue->last_timestamp.tv_nsec = 0;
+	queue->last_timestamp.tv_nsec = CSP_TIMESTAMP_INVALID_NSEC;
 	queue->client_timestamp.tv_sec = 0;
-	queue->client_timestamp.tv_nsec = 0;
+	queue->client_timestamp.tv_nsec = CSP_TIMESTAMP_INVALID_NSEC;
 }
 
 int param_queue_add(param_queue_t *queue, param_t *param, int offset, void *value) {
