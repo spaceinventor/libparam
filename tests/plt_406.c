0ea449c96b2331d80b8a7ace87a7f503791c4aa9
Jean-Baptiste Lab <jean.baptiste@space-inventor.com>
2026-07-24 15:01:52 +0200

    Improve tests a bit after review

--- tests/plt_406.c
+++ tests/plt_406.c
@@ -14,20 +14,22 @@ int main (int argc, char *argv[]) {
     const param_t * p = param_list_find_id(0, 1);
     if(NULL == p) {
         /* libparam is probably built as a shared library -> need to explicitly add our own parameters*/
-        param_list_add((param_t *)&plt_406);
+        param_t *local = param_list_create_remote(1, 0, PARAM_TYPE_INT32, PM_READONLY, 1, "plt_406", "-", "PLT 406 test parameter", -1);
+        assert(local);
+        param_list_add(local);
     }
     p = param_list_find_id(0, 1);
     assert(p);
 
-    /* Add a remote parameter with node = 0 */
+    /* Add a "fake" remote parameter with node = 0 */
     param_t *remote = param_list_create_remote(128, 0, PARAM_TYPE_UINT16, PM_DEBUG, 0, "remote", NULL, NULL, -1);
-    assert(p);
+    assert(remote);
     param_list_add(remote);
     p = param_list_find_id(0, 128);
     assert(p);
 
     /* Add a remote parameter with node != 0 */
-    remote = param_list_create_remote(128, 400, PARAM_TYPE_UINT16, PM_DEBUG, 0, "remote", NULL, NULL, -1);
+    remote = param_list_create_remote(128, 400, PARAM_TYPE_UINT16, PM_REMOTE, 0, "remote", NULL, NULL, -1);
     assert(remote);
     param_list_add(remote);
     p = param_list_find_id(400, 128);
